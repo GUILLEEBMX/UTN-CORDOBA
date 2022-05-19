98de1c6f292f50e1b8099dc6995a4b2da528c085
Guillee Britos <guillermo.britos@kdsoft.io>
2022-05-19 19:55:49 -0300

    Actualizando Main

---
+++ Problema 4.13 ABM/Problema 4.13 ABM/DBContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_4._13_ABM
+{
+    class DBContext
+    {
+
+
+        public DBContext()
+        {
+            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            SqlConnection context = new SqlConnection(connectionString);
+
+        }
+
+        
+
+
+
+
+
+    }
+}
