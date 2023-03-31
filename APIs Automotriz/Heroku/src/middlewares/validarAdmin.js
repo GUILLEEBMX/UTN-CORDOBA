896ca591cabe6d476cc68224db97d854f0434952
GUILLEEBMX <gbritos13@gmail.com>
2023-03-30 21:06:06 -0300

    - Test

--- APIs Automotriz/Heroku/src/middlewares/validarAdmin.js
+++ APIs Automotriz/Heroku/src/middlewares/validarAdmin.js
@@ -1,56 +1,56 @@
-const { response, request } = require('express');
-const jwt = require('jsonwebtoken');
-const authconfig = require('../config/auth')
-const { Usuarios } = require('../controllers/usersControllers')
+// const { response, request } = require('express');
+// const jwt = require('jsonwebtoken');
+// const authconfig = require('../config/auth')
+// const { Usuarios } = require('../controllers/usersControllers')
 
 
 
-async function ValidateAdmin(req, res, next) {
+// async function ValidateAdmin(req, res, next) {
 
-    let existe;
+//     let existe;
 
-    let response = {
-        body: null
-    }
+//     let response = {
+//         body: null
+//     }
 
-    const token = req.header('x-token');
+//     const token = req.header('x-token');
 
-    if (!token) {
+//     if (!token) {
 
-        response.body = "NO HAY TOKEN EN LA PETICION";
-        return res.json(response);
+//         response.body = "NO HAY TOKEN EN LA PETICION";
+//         return res.json(response);
 
-    }
+//     }
 
-    var decodedJwt = jwt.decode(token, { complete: true });
+//     var decodedJwt = jwt.decode(token, { complete: true });
 
-    let user = decodedJwt.payload.username;
+//     let user = decodedJwt.payload.username;
 
-    let usersGroups = await Usuarios.GellAllUsers("Administrator");
+//     let usersGroups = await Usuarios.GellAllUsers("Administrator");
 
-    usersGroups = usersGroups.map((x) => x.name);
+//     usersGroups = usersGroups.map((x) => x.name);
 
-    for (let index = 0; index < usersGroups.length; index++) {
+//     for (let index = 0; index < usersGroups.length; index++) {
 
-        if (user != usersGroups[index]) {
+//         if (user != usersGroups[index]) {
 
-            existe = true;
-        }
+//             existe = true;
+//         }
 
-    }
+//     }
 
-    if (!existe) {
+//     if (!existe) {
         
-        response.body = "EL USUARIO NO ES ADMINISTRADOR"
-        res.statusCode = 401;
-        res.json(response);
-        return response;
-    }
+//         response.body = "EL USUARIO NO ES ADMINISTRADOR"
+//         res.statusCode = 401;
+//         res.json(response);
+//         return response;
+//     }
 
 
 
 
-    next();
-}
+//     next();
+// }
 
-module.exports = { ValidateAdmin }
+// module.exports = { ValidateAdmin }
